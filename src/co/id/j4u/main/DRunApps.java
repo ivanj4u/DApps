141c859905929b6094300c985f24ffe06e80d26b
Ivan Aribanilia <angko.j4u@gmail.com>
2016-03-29 11:25:02 +0700

    Project Initialize Commit

---
+++ src/co/id/j4u/main/DRunApps.java
@@ -0,0 +1,171 @@
+package co.id.j4u.main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
+
+import co.id.j4u.util.DConstants;
+
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxAuthFinish;
+import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxEntry;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxWebAuthNoRedirect;
+import com.dropbox.core.DbxWriteMode;
+
+/**
+ * Aplikasi connect ke Dropbox
+ * @author ivan_j4u
+ */
+public class DRunApps {
+
+	public static void main(String[] args) {
+		File file = null;
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		DbxClient client = null;
+		try {
+			DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
+			client = getClient(config, DConstants.APP.TOKEN);
+//			doUploadFile(file, in, "read.txt", client, false);
+//			doDownloadFile(out, "read.txt", client);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static DbxClient getClient(DbxRequestConfig config, String token) {
+		return new DbxClient(config, DConstants.APP.TOKEN);
+	}
+	
+	private static void doListeningFolder(DbxClient client) throws Exception {
+		DbxEntry.WithChildren list = client.getMetadataWithChildren("/");
+		System.out.println("Files in the root path :");
+		for (DbxEntry child : list.children) {
+			System.out.println("  " + child.name + " : " + child.toString());
+		}
+	}
+	
+	private static void doUploadFile(File file, FileInputStream in, String fileName, DbxClient client, boolean isUpdate) throws IOException, DbxException {
+		File dir = new File(DConstants.DIR.UPLOAD);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		file = new File(dir.getAbsolutePath() + "/" + fileName);
+		in = new FileInputStream(file);
+		// Uploading File
+		DbxEntry.File uploadFile = client.uploadFile(DConstants.DIR.CONFIG + fileName, 
+				(isUpdate ? DbxWriteMode.update(null) : DbxWriteMode.add()),
+				file.length(), in);
+		System.out.println("Data Uploaded" + uploadFile.toString());
+		if (in != null)
+			in.close();
+	}
+
+	private static void doDownloadFile(FileOutputStream out, String fileName, DbxClient client) throws IOException, DbxException {
+		File dir = new File(DConstants.DIR.DOWNLOAD);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		out = new FileOutputStream(dir.getAbsolutePath() + "/" + fileName);
+		// Downloading file
+		DbxEntry.File downloadFile = client.getFile(DConstants.DIR.CONFIG + fileName, null, out);
+		System.out.println("Data Downloaded" + downloadFile.toString());
+		if (out != null)
+			out.close();
+	}
+	
+	private static void doReadFile(File file, FileInputStream in) throws IOException {
+		in = new FileInputStream(file);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		
+		String s;
+		while((s = reader.readLine()) != null) {
+			System.out.println("Reading..." + s);
+		}
+		if (reader != null)
+			reader.close();
+		if (in != null)
+			in.close();
+		System.out.println("File Done...");
+	}
+	
+	private static void doWriteFile(File file, FileOutputStream out, String s) throws IOException {
+		out = new FileOutputStream(file);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+		writer.write(s);
+		writer.flush();
+		System.out.println("Writing...");
+		if (writer != null)
+			writer.close();
+		if (out != null)
+			out.close();
+		System.out.println("File Done...");
+	}
+	
+	private static void doReadWrite(FileInputStream in, FileOutputStream out, String fileName) throws IOException {
+		File fileDownload = new File(DConstants.DIR.DOWNLOAD + fileName);
+		File fileUpload = new File(DConstants.DIR.UPLOAD + fileName);
+		if (!fileUpload.exists())
+			fileUpload.createNewFile();
+		in = new FileInputStream(fileDownload);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		out = new FileOutputStream(fileUpload);
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+		String s;
+		StringBuffer sb = new StringBuffer();
+		while ((s = reader.readLine())!= null) {
+			System.out.println("Reading..." + s);
+			sb.append(s);
+		}
+		writer.write(sb.toString());
+		writer.flush();
+		
+		System.out.println("Writing...");
+		if (reader != null)
+			reader.close();
+		if (writer != null)
+			writer.close();
+		if (in != null)
+			in.close();
+		if (out != null)
+			out.close();
+		System.out.println("File Done...");
+	}
+	
+	/**
+	 * Testing method DropBox
+	 */
+	private static void testing() throws IOException, DbxException {
+		DbxAppInfo appInfo = new DbxAppInfo(DConstants.APP.KEY, DConstants.APP.SECRET);
+		DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
+		
+		// Authorize by web
+		DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+		String authorizeUrl = webAuth.start();
+		// Have the user sign in and authorize this token
+		System.out.println("1. Go to this URL : " + authorizeUrl);
+		System.out.println("2. Click \"Allow\" (you might have to log in first)");
+		System.out.println("3. Copy the authorization code and paste here ");
+		String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+		System.out.println("4. Authorize Code : " + code);
+		
+		// Exchange code with access token (jika token belum digenerate)
+		DbxAuthFinish authFinish = webAuth.finish(code);
+		String accessToken = authFinish.accessToken;
+		System.out.println("5. Authorize Token : " + accessToken);
+		
+		// User Account (digunakan untuk upload dan download)
+		DbxClient client = new DbxClient(config, DConstants.APP.TOKEN);
+		System.out.println("Linked Account : " + client.getAccountInfo().displayName);
+	}
+	
+}
---
+++ src/co/id/j4u/util/DConstants.java
@@ -0,0 +1,28 @@
+package co.id.j4u.util;
+
+public class DConstants {
+	
+	public interface APP {
+		public String KEY = "53agjjxvhs6oiky";
+		public String SECRET = "lpv636aa7pmrck6";
+		public String TOKEN = "ou3dGNYdOK8AAAAAAAAAfsqzGLcNkUfZ0JM3Qjl3QHohsTY1KiX2gIiP7f-PeNKd";
+	}
+
+	public interface DIR {
+		public String DOWNLOAD = "D:/Test/Drop/download/";
+		public String UPLOAD = "D:/Test/Drop/upload/";
+		public String CONFIG = "/config/";
+	}
+	
+	public interface FILE {
+		public String USER = "user.properties";
+		public String APP = "app.properties";
+		public String TRX = "trx.properties";
+	}
+	
+	public interface CONFIG {
+		public String IS_LOGIN = "is-login";
+		public String LAST_LOGIN = "last-login";
+	}
+	
+}
